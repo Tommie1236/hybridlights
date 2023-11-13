a22fdc6ea88f96e23bdcf3a09eae8b1e5ab57b3e
tommie1236 <timooosterom@gmail.com>
2023-11-13 23:39:44 +0100

    fixed the nav bar scroll animation (a bit)

--- websrv/static/js/base.js
+++ websrv/static/js/base.js
@@ -4,10 +4,10 @@ console.log(navLogo);
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("navbar").style.padding = "5px 20px";
     navLogo.style.maxHeight = "50px";
 } else {
-    //document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("navbar").style.padding = "20px";
     navLogo.style.maxHeight = "100px";
   }
 } 
